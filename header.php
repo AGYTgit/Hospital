be3d9292972ede1e5dd07d6385be513699daf504
AGYTgit <bubonb12@gmail.com>
2024-05-30 17:42:44 +0200

    Version 1.0

---
+++ header.php
@@ -0,0 +1,13 @@
+<header>
+    <div class="header-left">
+        <a href="index.php"><h2>Hospital</h></a>
+    </div>
+    <div class="header-mid">
+        <a href="index.php"><h3>Patient</h3></a>
+        <a href="index2.php"><h3>Staff</h3></a>
+    </div>
+    <div class="header-right">
+        <a href="index3.php"><h3>Insert Patient</h3></a>
+        <a href="index4.php"><h3>Insert Staff</h3></a>
+    </div>
+</header>
